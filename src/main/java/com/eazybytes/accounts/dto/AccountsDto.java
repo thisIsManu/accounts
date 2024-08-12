440a119cbe328c6b66c7db70b98546469def6456
Manu Mishra <mishramanu26119972@gmail.com>
2024-08-12 22:28:45 +0530

    code refactored

--- src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
+++ src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -1,33 +1,19 @@
 package com.eazybytes.accounts.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
-@Schema(
-        name = "Accounts",
-        description = "Schema to hold Account information"
-)
 public class AccountsDto {
 
-    @NotEmpty(message = "AccountNumber can not be a null or empty")
-    @Pattern(regexp="(^$|[0-9]{10})",message = "AccountNumber must be 10 digits")
-    @Schema(
-            description = "Account Number of Eazy Bank account", example = "3454433243"
-    )
+    @Max(value = 9999999999L, message = "AccountNumber Must be of 10 digits")
+    @Min(value = 1000000000L,message = "AccountNumber Must be of 10 digits")
+    @NotNull
     private Long accountNumber;
 
-    @NotEmpty(message = "AccountType can not be a null or empty")
-    @Schema(
-            description = "Account type of Eazy Bank account", example = "Savings"
-    )
+    @NotEmpty(message = "AccountType can not be null or Empty")
     private String accountType;
 
-    @NotEmpty(message = "BranchAddress can not be a null or empty")
-    @Schema(
-            description = "Eazy Bank branch address", example = "123 NewYork"
-    )
+    @NotEmpty(message = "branchAddress can not be null or Empty")
     private String branchAddress;
 }
